5afc054647a32cf9fdf8ab2c462a865312616069
macstudent <macstudent@dhillss-iMac-100.local>
2018-10-26 17:54:20 -0700

    Finished except for a visual bug upon runtime

--- tumblr_feed/PhotosViewController.swift
+++ tumblr_feed/PhotosViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class PhotosViewController: UIViewController
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    
+    @IBOutlet weak var tableView: UITableView!
     var posts: [[String: Any]] = []
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -28,14 +34,46 @@ class PhotosViewController: UIViewController
                 print(dataDictionary)
                 
                 // TODO: Get the posts and store in posts property
-                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
                 // TODO: Reload the table view
+                self.tableView.reloadData()
             }
         }
         task.resume()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let post = posts[indexPath.row]
+        
+        // 1.            // 2.          // 3.
+        if let photos = post["photos"] as? [[String: Any]]
+        {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            // 1.
+            let photo = photos[0]
+            // 2.
+            let originalSize = photo["original_size"] as! [String: Any]
+            // 3.
+            let urlString = originalSize["url"] as! String
+            // 4.
+            let url = URL(string: urlString)
+            
+            cell.photoImageView.af_setImage(withURL: url!)
+        }
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
